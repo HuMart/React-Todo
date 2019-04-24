a6f8ea4f406a4a019aabedcf21a494715ec3194f
Manya0 <manya01@protonmail.com>
2019-04-24 16:12:37 -0400

    App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,22 @@
 import React from 'react';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      task: "",
+      id: Date.now(),
+      todos: [],
+      completed: false
+    }
+  }
+
+  handleChanges = event => {
+    this.setState({
+      task: event.target.value,
+      id: Date.now()
+    })
+  }
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
@@ -8,6 +24,7 @@ class App extends React.Component {
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
+        
       </div>
     );
   }
